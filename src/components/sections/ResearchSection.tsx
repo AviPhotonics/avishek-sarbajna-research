a71d50b4fcc9ec5ac12433448492a5af9c9d7884
Avishek Sarbajna <sarbajna.avishek.2@gmail.com>
2025-08-06 13:50:04 +0200

    Update ResearchSection.tsx

--- src/components/sections/ResearchSection.tsx
+++ src/components/sections/ResearchSection.tsx
@@ -50,7 +50,7 @@ const ResearchSection = () => {
                 role="Lead Researcher"
                 collaborators={["Prof. Søren Raza", "Dr. Daniel R. Danielsen"]}
                 funding="Laser Photonics Reviews publication"
-                image="/Picture2_7.png"
+                image="/0090 top DF.jpg"
                 links={[]}
                 tags={["Nanostructures", "Lossy Materials", "Field Confinement"]}
               />
